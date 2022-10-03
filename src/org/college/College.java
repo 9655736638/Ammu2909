9b3ad65b5a1c1d0e5d6fbe876392093501e65533
gitfirst <ammu07021994@gmail.com>
2022-10-03 13:05:27 +0530

    new code added

--- src/org/college/College.java
+++ src/org/college/College.java
@@ -3,6 +3,6 @@ package org.college;
 public class College extends Hostel {
 public void collegeName() {
 	System.out.println("College Name is GCT");
-
+    System.out.println("gct coimbatore");
 }
 }
